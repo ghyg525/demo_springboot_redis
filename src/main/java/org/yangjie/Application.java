b63d6161978208a02def470b50d6bbb58a77aee0
yangjie <yangjie@moko.cc>
2016-09-22 15:40:38 +0800

    升级springboot到1.4.0

--- src/main/java/org/yangjie/Application.java
+++ src/main/java/org/yangjie/Application.java
@@ -1,27 +1,26 @@
 package org.yangjie;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.yangjie.config.WebMvcConfig;
 
 /**
  * 程序入口, 启动器
+ * 
+ * 此注解等同与以下三个注解:
+ * @Configuration 此类为配置类
+ * @ComponentScan 扫描当前目录下所有包
+ * @EnableAutoConfiguration 开启根据依赖包进行自动配置
  */
 @SpringBootApplication
-@EnableAutoConfiguration
 public class Application {
 	
-	private static Logger logger = Logger.getLogger(Application.class);
+	private static Logger logger = LoggerFactory.getLogger(Application.class);
 
-	
 	public static void main(String[] args) {
-		
-		SpringApplication.run(WebMvcConfig.class, args);
-		
+		SpringApplication.run(Application.class, args);
 		logger.info("server is running...");
-		
 	}
  
 }
