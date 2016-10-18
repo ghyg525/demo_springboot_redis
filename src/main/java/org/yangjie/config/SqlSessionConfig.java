9d7d9c0055f6ed9d214f5cae4a1a1eda96bff4e2
yangjie <yangjie@moko.cc>
2016-10-18 11:48:02 +0800

    修改mybatis集成方式等

--- src/main/java/org/yangjie/config/SqlSessionConfig.java
+++ src/main/java/org/yangjie/config/SqlSessionConfig.java
@@ -2,41 +2,25 @@ package org.yangjie.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 @Configuration
-@MapperScan("org.yangjie.dao")
+@MapperScan("org.yangjie.dao") // 扫描dao接口, 自动生成代理实现
 public class SqlSessionConfig {
 
-	@Autowired
-	private DataSource dataSource;
-
-	
 	/**
-	 * 注册mybatis的SqlSessionFactoryBean
+	 * 注册mybatis的SqlSessionFactory
 	 * @return
 	 */
-	@Primary // 多实例时默认使用此实例
-	@Bean(name="sqlSessionFactory")
-	public SqlSessionFactoryBean getDBMoko() {
-		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
-		sqlSessionFactory.setDataSource(dataSource);
-		return sqlSessionFactory;
-	}
-	
-	/**
-	 * 注册spring事务管理器
-	 * @return
-	 */
-	@Bean
-	DataSourceTransactionManager getTransactionManager() {
-		return new DataSourceTransactionManager(dataSource);
+	@Bean // DataSource由springboot自动创建 (yml中必须配置spring.datasource)
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
+		sessionFactoryBean.setDataSource(dataSource); // 设置数据源
+		return sessionFactoryBean.getObject();
 	}
 	
 }
